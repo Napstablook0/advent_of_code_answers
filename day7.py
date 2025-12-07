433d5ad22259bb0f0e2c8b5326ed41b8fbe245ba
HUC Raphael <168347531+Napstablook0@users.noreply.github.com>
2025-12-07 15:57:26 +0100

    Update day7.py

--- day7.py
+++ day7.py
@@ -41,15 +41,50 @@ def moveDownward(mat, x, y):
     return _moveDownward(mat, x, y, deja_faits)
 
 
+def _moveDownwardLevel2(mat:  list[str], x: int, y: int, deja_faits: dict):
+    """returns the number of times the beam splited"""
+    n  = len(mat)
+    m =  len(mat[0])
+    assert x >= 0 and x < m, "x has to be a correct coordinate"
+    assert y >= 0 and y < n, "y has to be a correct coordinate"
+
+
+    if (x, y) in deja_faits.keys():
+        return deja_faits[(x, y)]
+
+    if (mat[y][x] == "." or mat[y][x] == "S") and y+1 >= 0 and y+1 < n:
+        next = _moveDownwardLevel2(mat, x, y+1, deja_faits)
+        deja_faits[(x, y+1)] = next
+        
+        return next
+    elif mat[y][x] == "^":
+        next1 = _moveDownwardLevel2(mat, x-1, y, deja_faits)
+        next2 = _moveDownwardLevel2(mat, x+1, y, deja_faits)
+        deja_faits[(x-1, y)] = next1
+        deja_faits[(x+1, y)] = next2
+
+        return next1 + next2
+    else:
+        return 1
+    
+
+
+def moveDownwardLevel2(mat, x, y):
+    deja_faits = {}
+    return _moveDownwardLevel2(mat, x, y, deja_faits)
+
+
 
 with open("input7_test.txt", "r") as f:
     inp_test = f.read().splitlines()
 
 
-assert moveDownward(inp_test, 7, 0) == 21, "tests"
+assert moveDownwardLevel2(inp_test, 7, 0) == 40, "tests"
 
 
 
 
-result = moveDownward(inp, 70, 0)
-print(result)
+result1 = moveDownward(inp, 70, 0)
+result2 = moveDownwardLevel2(inp, 70, 0)
+print("level1 :", result1)
+print("level2 :", result2)
